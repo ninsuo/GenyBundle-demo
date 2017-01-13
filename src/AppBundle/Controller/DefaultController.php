fd1cb3d6ddc27d4e5d5424bf47d265c60e99fa5e
Gilles Lengy <gilles.lengy@artaban.fr>
2017-01-13 10:25:17 +0100

    View to update persisted data form a form with text only

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -127,14 +127,105 @@ class DefaultController extends BaseController {
             }
 
             $em->flush();
-            
+
             $request->getSession()->getFlashBag()->add('notice', 'Data Persisted');
-            
+
             return $this->redirectToRoute('view_data', array('id_form' => $id, 'id' => $set_id));
         }
 
         return [
             'id' => $id,
+            'form' => $form->createView(),
+            'data' => $data,
+        ];
+    }
+
+    /**
+     * @Route(
+     * "/update/data/form/{id_form}/{id_data}",
+     *  name="update_data_form",
+     *  requirements = {
+     *     "id_form" = "^\d+$",
+     *     "id_data" = "^\d+$",
+     *                }
+     * )
+     * @Template()
+     */
+    public function updateDataFormAction(Request $request, $id_form, $id_data) {
+
+        // Init
+        $set_id = 0;
+
+        // Check if form exists here
+
+
+        $em = $this->getDoctrine()
+                ->getEntityManager();
+
+
+        $data_set = $em->getRepository('GenyBundle:Data')
+                ->dataSet($id_data)
+        ;
+
+        $overloadOptions = array();
+        foreach ($data_set as $data_to_catched) {
+               $overloadOptions[$data_to_catched['name']]=array('data' => $data_to_catched['text']);
+        }
+
+        $form = $this->get('geny')->getForm($id_form, $overloadOptions);
+        $form->handleRequest($request);
+
+        $data = null;
+        //$data= array('question_1'=>'rep1','question_2' => 'rep2');
+        if ($form->isValid()) {
+            $data = $form->getData();
+        }
+
+          // To be done by a function. From a provider ? From a Repo ? From the geny service ?
+          if ($data) {
+          $em = $this->getDoctrine()->getManager();
+          foreach ($data as $data_key => $data_value) {
+
+          $field = $em->getRepository('GenyBundle:Field')->findOneByName($data_key);
+          $field_type = $field->getType();
+
+          switch ($field_type) {
+          case "text":
+          $data_text = new DataText;
+          $data_text->setText($data_value);
+          $data_text->setUpdatedAt(new \Datetime());
+          $em->persist($data_text);
+          $em->flush();
+
+          $data_object = new Data();
+          $data_object->setFieldID($field);
+          $data_object->setUpdatedAt(new \Datetime());
+          $em->persist($data_object);
+          $em->flush();
+
+          $data_id = $data_object->getId();
+          if ($set_id == 0) {
+          $set_id = $data_id;
+          }
+
+          $data_object->setDataTextID($data_text);
+
+          $data_object->setSetID($set_id);
+          $em->persist($data_object);
+          $em->flush();
+          break;
+          }
+          }
+
+          $em->flush();
+
+          $request->getSession()->getFlashBag()->add('notice', 'Data Persisted');
+
+          return $this->redirectToRoute('view_data', array('id_form' => $id_form, 'id' => $set_id));
+          }
+
+        return [
+            'id' => $id_form,
             'form' => $form->createView(),
             'data' => $data,
         ];
@@ -152,13 +243,13 @@ class DefaultController extends BaseController {
      */
     public function viewSetDataFormAction(Request $request, $id_form, $id) {
         ini_set('display_errors', 1);
-        
+
 
         $em = $this->getDoctrine()
                 ->getEntityManager();
-        
+
         $form = $em->getRepository('GenyBundle:Form')->findOneById($id_form);
-        
+
 
 
         $data = $em->getRepository('GenyBundle:Data')
@@ -171,8 +262,8 @@ class DefaultController extends BaseController {
             'data' => $data
         ];
     }
-    
-        /**
+
+    /**
      * @Route(
      * "/view/data/{id}",
      *  name="view_list_data_set",
@@ -184,17 +275,17 @@ class DefaultController extends BaseController {
      */
     public function viewListSetDataFormAction(Request $request, $id) {
         ini_set('display_errors', 1);
-        
+
         $em = $this->getDoctrine()
                 ->getEntityManager();
-        
+
         $form = $em->getRepository('GenyBundle:Form')->findOneById($id);
-        
+
         $data_set_list = $em->getRepository('GenyBundle:Data')
                 ->dataSetList($id)
         ;
-        
-        
+
+
         return [
             'id' => $id,
             'form' => $form,
