12719c73c735704ebd8726e9f764c1f58320fff7
Gilles Lengy <gilles.lengy@artaban.fr>
2017-01-24 15:22:37 +0100

    ItemType generated via command line

---
+++ src/GL/ItemBundle/Form/ItemType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace GL\ItemBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ItemType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('updatedAt')
+            ->add('createdAt')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'GL\ItemBundle\Entity\Item'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'gl_itembundle_item';
+    }
+}
